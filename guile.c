7aab32a7b8670d4121c550ef91aef850eea71dd3
gtw <gtw>
2000-11-01 15:09:42 +0000

    New Guile functions cube-info, cube-info-match and cube-info-money.

--- guile.c
+++ guile.c
@@ -65,12 +65,119 @@ static void SCMToBoard( SCM s, int anBoard[ 2 ][ 25 ] ) {
     }
 }
 
+static SCM CubeInfoToSCM( cubeinfo *pci ) {
+
+    SCM s = scm_make_vector( SCM_MAKINUM( 7 ), SCM_UNSPECIFIED );
+
+    scm_vector_set_x( s, SCM_MAKINUM( 0 ), SCM_MAKINUM( pci->nCube ) );
+    scm_vector_set_x( s, SCM_MAKINUM( 1 ), SCM_MAKINUM( pci->fCubeOwner ) );
+    scm_vector_set_x( s, SCM_MAKINUM( 2 ), SCM_MAKINUM( pci->fMove ) );
+    scm_vector_set_x( s, SCM_MAKINUM( 3 ),
+		      scm_make_real( pci->arGammonPrice[ 0 ] ) );
+    scm_vector_set_x( s, SCM_MAKINUM( 4 ),
+		      scm_make_real( pci->arGammonPrice[ 1 ] ) );
+    scm_vector_set_x( s, SCM_MAKINUM( 5 ),
+		      scm_make_real( pci->arGammonPrice[ 2 ] ) );
+    scm_vector_set_x( s, SCM_MAKINUM( 6 ),
+		      scm_make_real( pci->arGammonPrice[ 3 ] ) );
+
+    return s;
+}
+
+static void SCMToCubeInfo( SCM s, cubeinfo *pci ) {
+
+    SCM sv;
+    int i;
+    
+    sv = scm_vector_ref( s, SCM_MAKINUM( 0 ) );
+    SCM_ASSERT( SCM_INUMP( sv ), sv, SCM_ARGn, NULL );
+    pci->nCube = SCM_INUM( sv );
+
+    sv = scm_vector_ref( s, SCM_MAKINUM( 1 ) );
+    SCM_ASSERT( SCM_INUMP( sv ), sv, SCM_ARGn, NULL );
+    pci->fCubeOwner = SCM_INUM( sv );
+
+    sv = scm_vector_ref( s, SCM_MAKINUM( 2 ) );
+    SCM_ASSERT( SCM_INUMP( sv ), sv, SCM_ARGn, NULL );
+    pci->fMove = SCM_INUM( sv );
+
+    for( i = 0; i < 4; i++ ) {
+	sv = scm_vector_ref( s, SCM_MAKINUM( i + 3 ) );
+	SCM_ASSERT( SCM_REALP( sv ), sv, SCM_ARGn, NULL );
+	pci->arGammonPrice[ i ] = SCM_REAL_VALUE( sv );
+    }
+}
+
 static SCM board_to_position_id( SCM sBoard ) {
 
     int anBoard[ 2 ][ 25 ];
     
     SCMToBoard( sBoard, anBoard );
     return scm_makfrom0str( PositionID( anBoard ) );
+}
+
+static SCM cube_info( SCM sCube, SCM sCubeOwner, SCM sMove ) {
+
+    cubeinfo ci;
+    static char sz[] = "cube-info";
+    
+    SCM_ASSERT( SCM_INUMP( sCube ) || sCube == SCM_UNDEFINED, sCube,
+		SCM_ARG1, sz );
+    SCM_ASSERT( SCM_INUMP( sCubeOwner ) || sCubeOwner == SCM_UNDEFINED,
+		sCubeOwner, SCM_ARG2, sz );
+    SCM_ASSERT( SCM_INUMP( sMove ) || sMove == SCM_UNDEFINED, sMove,
+		SCM_ARG3, sz );
+
+    if( sMove == SCM_UNDEFINED && fTurn < 0 )
+	/* no move specified, and no game in progress */
+	return SCM_BOOL_F;
+    
+    SetCubeInfo( &ci, sCube == SCM_UNDEFINED ? nCube : SCM_INUM( sCube ),
+		 sCubeOwner == SCM_UNDEFINED ? fCubeOwner :
+		 SCM_INUM( sCubeOwner ),
+		 sMove == SCM_UNDEFINED ? fMove : SCM_INUM( sMove ) );
+
+    return CubeInfoToSCM( &ci );
+}
+
+static SCM cube_info_match( SCM sCube, SCM sCubeOwner, SCM sMove,
+			    SCM sMatchTo, SCM sScore ) {
+    cubeinfo ci;
+    static char sz[] = "cube-info-match";
+    int an[ 2 ];
+    
+    SCM_ASSERT( SCM_INUMP( sCube ), sCube, SCM_ARG1, sz );
+    SCM_ASSERT( SCM_INUMP( sCubeOwner ), sCubeOwner, SCM_ARG2, sz );
+    SCM_ASSERT( SCM_INUMP( sMove ), sMove, SCM_ARG3, sz );
+    SCM_ASSERT( SCM_INUMP( sMatchTo ), sMatchTo, SCM_ARG4, sz );
+    SCM_ASSERT( SCM_CONSP( sScore ), sScore, SCM_ARG5, sz );
+    SCM_ASSERT( SCM_INUMP( SCM_CAR( sScore ) ), sScore, SCM_ARG5, sz );
+    SCM_ASSERT( SCM_INUMP( SCM_CDR( sScore ) ), sScore, SCM_ARG5, sz );
+
+    an[ 0 ] = SCM_INUM( SCM_CAR( sScore ) );
+    an[ 1 ] = SCM_INUM( SCM_CDR( sScore ) );
+    
+    SetCubeInfoMatch( &ci, SCM_INUM( sCube ), SCM_INUM( sCubeOwner ),
+		      SCM_INUM( sMove ), SCM_INUM( sMatchTo ), an );
+
+    return CubeInfoToSCM( &ci );
+}
+
+static SCM cube_info_money( SCM sCube, SCM sCubeOwner, SCM sMove,
+			    SCM sJacoby ) {
+
+    cubeinfo ci;
+    static char sz[] = "cube-info-money";
+
+    SCM_ASSERT( SCM_INUMP( sCube ), sCube, SCM_ARG1, sz );
+    SCM_ASSERT( SCM_INUMP( sCubeOwner ), sCubeOwner, SCM_ARG2, sz );
+    SCM_ASSERT( SCM_INUMP( sMove ), sMove, SCM_ARG3, sz );
+
+    SetCubeInfoMoney( &ci, SCM_INUM( sCube ), SCM_INUM( sCubeOwner ),
+		      SCM_INUM( sMove ), sJacoby == SCM_UNDEFINED ?
+		      fJacoby : SCM_NFALSEP( sJacoby ) );
+		      
+    return CubeInfoToSCM( &ci );
 }
 
 static SCM current_board( void ) {
@@ -83,9 +190,17 @@ static SCM evaluate_position( SCM sBoard, SCM sCube, SCM sEvalContext ) {
     int i, anBoard[ 2 ][ 25 ];
     float ar[ NUM_OUTPUTS ];
     SCM s;
+    cubeinfo ci;
     
     SCMToBoard( sBoard, anBoard );
-    EvaluatePosition( anBoard, ar, NULL, NULL ); /* FIXME use all params */
+    
+    if( sCube == SCM_UNDEFINED )
+	sCube = cube_info( SCM_UNDEFINED, SCM_UNDEFINED, SCM_UNDEFINED );
+
+    SCMToCubeInfo( sCube, &ci );
+ 
+    if( EvaluatePosition( anBoard, ar, &ci, NULL /* FIXME */ ) < 0 )
+	return SCM_BOOL_F; /* FIXME throw error? */
     
     s = scm_make_vector( SCM_MAKINUM( NUM_OUTPUTS ), SCM_UNSPECIFIED );
     for( i = 0; i < NUM_OUTPUTS; i++ )
@@ -121,23 +236,26 @@ static SCM rollout_position( SCM sBoard, SCM sGames, SCM sTruncate,
     cubeinfo ci;
     static char sz[] = "rollout-position";
     
-    SCM_ASSERT( SCM_INUMP( sGames ) || sGames == SCM_UNSPECIFIED, sGames,
+    SCM_ASSERT( SCM_INUMP( sGames ) || sGames == SCM_UNDEFINED, sGames,
 		SCM_ARG2, sz );
-    SCM_ASSERT( SCM_INUMP( sTruncate ) || sTruncate == SCM_UNSPECIFIED,
+    SCM_ASSERT( SCM_INUMP( sTruncate ) || sTruncate == SCM_UNDEFINED,
 		sTruncate, SCM_ARG3, sz );
-    SCM_ASSERT( SCM_BOOLP( sVarRedn ) || sVarRedn == SCM_UNSPECIFIED,
+    SCM_ASSERT( SCM_BOOLP( sVarRedn ) || sVarRedn == SCM_UNDEFINED,
 		sVarRedn, SCM_ARG4, sz );
     
     SCMToBoard( sBoard, anBoard );
-    
-    SetCubeInfo ( &ci, nCube, fCubeOwner, fMove ); /* FIXME use sCube */
+
+    if( sCube == SCM_UNDEFINED )
+	sCube = cube_info( SCM_UNDEFINED, SCM_UNDEFINED, SCM_UNDEFINED );
+
+    SCMToCubeInfo( sCube, &ci );
  
-    if( Rollout( anBoard, ar, arStdDev, sTruncate == SCM_UNSPECIFIED ?
+    if( Rollout( anBoard, ar, arStdDev, sTruncate == SCM_UNDEFINED ?
 		 nRolloutTruncate : SCM_INUM( sTruncate ), sGames ==
-		 SCM_UNSPECIFIED ? nRollouts : SCM_INUM( sGames ),
+		 SCM_UNDEFINED ? nRollouts : SCM_INUM( sGames ),
 		 SCM_NFALSEP( sVarRedn ), &ci,
 		 &ecRollout /* FIXME use sEvalContext */ ) < 0 )
-	return SCM_UNSPECIFIED; /* FIXME throw error? */
+	return SCM_BOOL_F; /* FIXME throw error? */
 
     s = scm_cons( scm_make_vector( SCM_MAKINUM( NUM_ROLLOUT_OUTPUTS ),
 				   SCM_UNSPECIFIED ),
@@ -156,6 +274,9 @@ static SCM rollout_position( SCM sBoard, SCM sGames, SCM sTruncate,
 extern int GuileInitialise( void ) {
 
     scm_make_gsubr( "board->position-id", 1, 0, 0, board_to_position_id );
+    scm_make_gsubr( "cube-info", 0, 3, 0, cube_info );
+    scm_make_gsubr( "cube-info-match", 5, 0, 0, cube_info_match );
+    scm_make_gsubr( "cube-info-money", 3, 1, 0, cube_info_money );
     scm_make_gsubr( "current-board", 0, 0, 0, current_board );
     scm_make_gsubr( "evaluate-position", 1, 2, 0, evaluate_position );
     scm_make_gsubr( "position-id->board", 1, 0, 0, position_id_to_board );
