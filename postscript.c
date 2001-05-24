abfb0151efc597c7698b5efaba2726733b8aef80
gtw <gtw>
2001-05-24 17:06:30 +0000

    More work on PostScript exporting.

--- postscript.c
+++ postscript.c
@@ -29,12 +29,16 @@
 #include "backgammon.h"
 #include "drawboard.h"
 
+typedef enum _font { FONT_NONE, FONT_RM, FONT_SF, FONT_TT } font;
+static char *aszFont[ FONT_TT + 1 ] = { NULL, "rm", "sf", "tt" };
+
 /* Output settings.  FIXME there should be commands to modify these! */
 static int cxPage = 595, cyPage = 842; /* A4 -- min (451,648) */
 static int nMag = 100; /* Board scale (percentage) -- min 1, max 127 */
 
 /* Current document state. */
-static int iPage, y;
+static int iPage, y, nFont;
+static font fn;
 
 static void PostScriptEscape( FILE *pf, char *pch ) {
 
@@ -53,7 +57,7 @@ static void PostScriptEscape( FILE *pf, char *pch ) {
 	    if( (unsigned char) *pch >= 0x80 )
 		fprintf( pf, "\\%030o", *pch );
 	    else
-		fputc( *pch, pf );
+		putc( *pch, pf );
 	    break;
 	}
 	pch++;
@@ -63,19 +67,28 @@ static void PostScriptEscape( FILE *pf, char *pch ) {
 static void StartPage( FILE *pf ) {
 
     iPage++;
+    fn = FONT_NONE;
+    y = 648;
     
     fprintf( pf, "%%%%Page: %d %d\n"
 	     "%%%%BeginPageSetup\n"
-	     "/pageobj save def %d %d translate 10 rm\n"
+	     "/pageobj save def %d %d translate\n"
 	     "%%%%EndPageSetup\n", iPage, iPage,
 	     ( cxPage - 451 ) / 2, ( cyPage - 648 ) / 2 );
-
-    y = 648;
 }
 
 static void EndPage( FILE *pf ) {
 
     fputs( "pageobj restore showpage\n", pf );
+}
+
+static void RequestFont( FILE *pf, font fnNew, int nFontNew ) {
+
+    if( fnNew != fn || nFontNew != nFont ) {
+	fn = fnNew;
+	nFont = nFontNew;
+	fprintf( pf, "%d %s\n", nFont, aszFont[ fn ] );
+    }
 }
 
 static void Ensure( FILE *pf, int cy ) {
@@ -101,8 +114,20 @@ static void Advance( FILE *pf, int cy ) {
     Consume( pf, cy );
 }
 
+static void Skip( FILE *pf, int cy ) {
+
+    if( y < cy ) {
+	EndPage( pf );
+	StartPage( pf );
+    } else if( y != 648 )
+	Consume( pf, cy );
+}
+
 static void PostScriptPrologue( FILE *pf ) {
 
+    /* FIXME change the font-setting procedures to use ISO 8859-1
+       encoding */
+    
     static char szPrologue[] =
 	"%%Creator: (GNU Backgammon " VERSION ")\n"
 	"%%DocumentData: Clean7Bit\n"
@@ -306,11 +331,137 @@ static void PrintPostScriptBoard( FILE *pf, matchstate *pms, int fPlayer ) {
     fputs( "grestore\n", pf );
 }
 
+/* Typeset a line of text.  We're not TeX, so we don't do any kerning,
+   hyphenation or justification.  Word wrapping will have to do. */
+static void PrintPostScriptComment( FILE *pf, unsigned char *pch ) {
+
+    static short acx[ 256 ] = {
+	/* Width of Times-Roman font in ISO 8859-1 encoding. */
+	250, 250, 250, 250, 250, 250, 250, 250, /*   0 to   7 */
+	250, 250, 250, 250, 250, 250, 250, 250, /*   8 to  15 */
+	250, 250, 250, 250, 250, 250, 250, 250, /*  16 to  23 */
+	250, 250, 250, 250, 250, 250, 250, 250, /*  24 to  31 */
+	250, 333, 408, 500, 500, 833, 778, 333, /*  32 to  39 */
+	333, 333, 500, 564, 250, 333, 250, 278, /*  40 to  47 */
+	500, 500, 500, 500, 500, 500, 500, 500, /*  48 to  55 */
+	500, 500, 278, 278, 564, 564, 564, 444, /*  56 to  63 */
+	921, 722, 667, 667, 722, 611, 556, 722, /*  64 to  71 */
+	722, 333, 389, 722, 611, 889, 722, 722, /*  72 to  79 */
+	556, 722, 667, 556, 611, 722, 722, 944, /*  80 to  87 */
+	722, 722, 611, 333, 278, 333, 469, 500, /*  88 to  95 */
+	333, 444, 500, 444, 500, 444, 333, 500, /*  96 to 103 */
+	500, 278, 278, 500, 278, 778, 500, 500, /* 104 to 111 */
+	500, 500, 333, 389, 278, 500, 500, 722, /* 112 to 119 */
+	500, 500, 444, 480, 200, 480, 541, 250, /* 120 to 127 */
+	250, 250, 250, 250, 250, 250, 250, 250, /* 128 to 135 */
+	250, 250, 250, 250, 250, 250, 250, 250, /* 136 to 143 */
+	250, 250, 250, 250, 250, 250, 250, 250, /* 144 to 151 */
+	250, 250, 250, 250, 250, 250, 250, 250, /* 152 to 159 */
+	250, 333, 500, 500, 500, 500, 200, 500, /* 160 to 167 */
+	333, 760, 276, 500, 564, 333, 760, 333, /* 168 to 175 */
+	400, 564, 300, 300, 333, 500, 453, 250, /* 176 to 183 */
+	333, 300, 310, 500, 750, 750, 750, 444, /* 184 to 191 */
+	722, 722, 722, 722, 722, 722, 889, 667, /* 192 to 199 */
+	611, 611, 611, 611, 333, 333, 333, 333, /* 200 to 207 */
+	722, 722, 722, 722, 722, 722, 722, 564, /* 208 to 215 */
+	722, 722, 722, 722, 722, 722, 556, 500, /* 216 to 223 */
+	444, 444, 444, 444, 444, 444, 667, 444, /* 224 to 231 */
+	444, 444, 444, 444, 278, 278, 278, 278, /* 232 to 239 */
+	500, 500, 500, 500, 500, 500, 500, 564, /* 240 to 247 */
+	500, 500, 500, 500, 500, 500, 500, 500  /* 248 to 255 */
+    };
+    int x;
+    unsigned char *pchStart, *pchBreak;
+    
+    if( !pch || !*pch )
+	return;
+
+    Skip( pf, 6 );
+
+    while( *pch ) {
+	Advance( pf, 10 );
+	
+	pchBreak = NULL;
+	pchStart = pch;
+	
+	for( x = 0; x < 451 * 1000 / 10; x += acx[ *pch++ ] )
+	    if( !*pch ) {
+		/* finished; break here */
+		pchBreak = pch;
+		break;
+	    } else if( *pch == '\n' ) {
+		/* new line; break immediately */
+		pchBreak = pch++;
+		break;
+	    } else if( *pch == ' ' )
+		/* space; note candidate for breaking */
+		pchBreak = pch;
+
+	if( !pchBreak )
+	    pchBreak = ( pch == pchStart ) ? pch : pch - 1;
+
+	RequestFont( pf, FONT_RM, 10 );
+	fprintf( pf, "0 %d moveto (", y );
+	for( ; pchStart <= pchBreak; pchStart++ )
+	    switch( *pchStart ) {
+	    case 0:
+	    case '\n':
+		break;
+	    case '\\':
+	    case '(':
+	    case ')':
+		putc( '\\', pf );
+		/* fall through */
+	    default:
+		putc( *pchStart, pf );
+	    }
+	fputs( ") show\n", pf );
+
+	if( !*pch )
+	    break;
+	
+	pch = pchBreak + 1;
+    }
+}
+
+static void PrintPostScriptCubeAnalysis( FILE *pf, matchstate *pms,
+					 int fPlayer, float arDouble[ 4 ],
+					 evalsetup *pes ) { 
+    cubeinfo ci;
+    char sz[ 1024 ], *pch, *pchNext;
+
+    if( pes->et == EVAL_NONE )
+	return;
+    
+    SetCubeInfo( &ci, pms->nCube, pms->fCubeOwner, fPlayer, pms->nMatchTo,
+		 pms->anScore, pms->fCrawford, fJacoby, fBeavers );
+    
+    if( !GetDPEq( NULL, NULL, &ci ) )
+	/* No cube action possible */
+	return;
+    
+    GetCubeActionSz( arDouble, sz, &ci, fOutputMWC, FALSE );
+
+    Skip( pf, 4 );
+    for( pch = sz; pch && *pch; pch = pchNext ) {
+	Advance( pf, 8 );
+	RequestFont( pf, FONT_TT, 8 );
+	fprintf( pf, "144 %d moveto (", y );
+	if( ( pchNext = strchr( pch, '\n' ) ) )
+	    *pchNext++ = 0;
+	fputs( pch, pf );
+	fputs( ") show\n", pf );
+    }
+    Skip( pf, 6 );
+}
+
 static void ExportGamePostScript( FILE *pf, list *plGame ) {
 
     list *pl;
     moverecord *pmr;
     matchstate msExport;
+    int fTook = FALSE, i;
+    char sz[ 1024 ], *pch;
 
     for( pl = plGame->plNext; pl != plGame; pl = pl->plNext ) {
 	pmr = pl->p;
@@ -320,35 +471,108 @@ static void ExportGamePostScript( FILE *pf, list *plGame ) {
 	    break;
 	    
 	case MOVE_NORMAL:
-	{
-	    /* FIXME this is a temporary hack */
-	    char sz[ 128 ];
-
-	    Ensure( pf, 260 * nMag / 100 + 10 );
-	    PrintPostScriptBoard( pf, &msExport, pmr->n.fPlayer );
-
+	    if( fTook )
+		/* no need to print board following a double/take */
+		fTook = FALSE;
+	    else {
+		Ensure( pf, 260 * nMag / 100 + 10 );
+		PrintPostScriptBoard( pf, &msExport, pmr->n.fPlayer );
+	    }
+	    
+	    PrintPostScriptCubeAnalysis( pf, &msExport, pmr->n.fPlayer,
+					 pmr->n.arDouble, &pmr->n.esDouble );
+	    
 	    Consume( pf, 10 );
 	    FormatMove( sz, msExport.anBoard, pmr->n.anMove );
-	    fprintf( pf, "225 %d moveto (%d%d%s: %s) cshow\n", y,
+	    RequestFont( pf, FONT_RM, 10 );
+	    fprintf( pf, "225 %d moveto (%d%d%s: %s%s) cshow\n", y,
 		     pmr->n.anRoll[ 0 ], pmr->n.anRoll[ 1 ],
-		     aszLuckTypeAbbr[ pmr->n.lt ], sz );
-	}
+		     aszLuckTypeAbbr[ pmr->n.lt ], sz,
+		     aszSkillTypeAbbr[ pmr->n.st ] );
+
+	    if( pmr->n.ml.cMoves ) {
+		Skip( pf, 4 );
+		for( i = 0; i < pmr->n.ml.cMoves && i <= pmr->n.iMove; i++ ) {
+		    if( i >= 5 /* FIXME allow user to choose limit */ &&
+			i != pmr->n.iMove )
+			continue;
+
+		    Ensure( pf, 16 );
+		    Consume( pf, 8 );
+		    RequestFont( pf, FONT_TT, 8 );
+		    fprintf( pf, "46 %d moveto (", y );
+		    putc( i == pmr->n.iMove ? '*' : ' ', pf );
+		    FormatMoveHint( sz, msExport.anBoard, &pmr->n.ml, i,
+				    i != pmr->n.iMove ||
+				    i != pmr->n.ml.cMoves - 1 );
+		    pch = strchr( sz, '\n' );
+		    *pch++ = 0;
+		    fputs( sz, pf );
+		    fputs( ") show\n", pf );
+		    
+		    Consume( pf, 8 );
+		    fprintf( pf, "46 %d moveto (", y );
+		    putc( ' ', pf );
+		    fputs( pch, pf );
+		    fputs( ") show\n", pf );
+		}
+	    }
+
+	    PrintPostScriptComment( pf, pmr->a.sz );
+
+	    Skip( pf, 6 );
+	    
 	    break;
 	    
 	case MOVE_DOUBLE:	    
-	    /* FIXME */
+	    Ensure( pf, 260 * nMag / 100 );
+	    PrintPostScriptBoard( pf, &msExport, pmr->d.fPlayer );
+
+	    PrintPostScriptCubeAnalysis( pf, &msExport, pmr->d.fPlayer,
+					 pmr->d.arDouble, &pmr->d.esDouble );
+
+	    Advance( pf, 10 );
+	    RequestFont( pf, FONT_RM, 10 );
+	    fprintf( pf, "225 %d moveto (Double) cshow\n", y );
+
+	    PrintPostScriptComment( pf, pmr->a.sz );
+
+	    Skip( pf, 6 );
+	    
 	    break;
 	    
 	case MOVE_TAKE:
-	    /* FIXME */
+	    fTook = TRUE;
+
+	    Advance( pf, 10 );
+	    RequestFont( pf, FONT_RM, 10 );
+	    fprintf( pf, "225 %d moveto (Take) cshow\n", y );
+
+	    PrintPostScriptComment( pf, pmr->a.sz );
+
+	    Skip( pf, 6 );
+	    
 	    break;
 	    
 	case MOVE_DROP:
-	    /* FIXME */
+	    Advance( pf, 10 );
+	    RequestFont( pf, FONT_RM, 10 );
+	    fprintf( pf, "225 %d moveto (Drop) cshow\n", y );
+
+	    PrintPostScriptComment( pf, pmr->a.sz );
+
+	    Skip( pf, 6 );
+
 	    break;
 	    
 	case MOVE_RESIGN:
-	    /* FIXME */
+	    /* FIXME print board? */
+	    /* FIXME print resign */
+	    /* FIXME print resignation analysis, if available */
+	    PrintPostScriptComment( pf, pmr->a.sz );
+
+	    Skip( pf, 6 );
+
 	    break;
 	    	    
 	case MOVE_SETDICE:
