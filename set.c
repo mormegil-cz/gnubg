8644b6cea5a37a81b7a9e425f30d699d5bda54f1
gtw <gtw>
2002-01-09 17:52:16 +0000

    #include <limits.h> wherever it is available, not just for Suns.

--- set.c
+++ set.c
@@ -24,16 +24,12 @@
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
+#if HAVE_LIMITS_H
+#include <limits.h>
+#endif
 #include <math.h>
 #if HAVE_SYS_SOCKET_H
 #include <sys/types.h>
-
-#if defined(sun)
-#define AF_LOCAL AF_UNIX
-#define PF_LOCAL PF_UNIX
-#include <limits.h>
-#endif
-
 #include <sys/socket.h>
 #include <sys/un.h>
 #endif
