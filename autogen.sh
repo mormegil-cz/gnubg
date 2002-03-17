7d8e1193cbcdfd730b2560b95c2f05319988538d
gtw <gtw>
2002-03-17 02:59:19 +0000

    Add an autogen.sh file to avoid regenerating autoconf/automake things.

---
+++ autogen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ ! -f gnubg.c ]; then
+    echo You must run autogen.sh in the top level source directory.
+    exit 1
+fi
+
+touch Makefile.in aclocal.m4 config.h.in configure
+touch doc/Makefile.in
+touch ext/Makefile.in
+touch lib/Makefile.in
+touch met/Makefile.in
