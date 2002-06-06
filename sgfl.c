5d82efcc0154900cc3ba4c77eca5f5a3def8713b
gtw <gtw>
2002-06-06 18:37:09 +0000

    Update automatically generated files derived from last commit.

--- sgfl.c
+++ sgfl.c
@@ -27,8 +27,7 @@
 #define YY_FLEX_MINOR_VERSION 5
 
 #include <stdio.h>
-#include <unistd.h>
-
+#include <errno.h>
 
 /* cfront 1.2 defines "c_plusplus" instead of "__cplusplus" */
 #ifdef c_plusplus
@@ -41,6 +40,15 @@
 #ifdef __cplusplus
 
 #include <stdlib.h>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#ifndef YY_ALWAYS_INTERACTIVE
+#ifndef YY_NEVER_INTERACTIVE
+extern int isatty YY_PROTO(( int ));
+#endif
+#endif
+#endif
 
 /* Use prototypes in function declarations. */
 #define YY_USE_PROTOS
@@ -956,7 +964,7 @@ static int error( char *s ) {
 #define YY_NO_SCAN_STRING 1
 #define value 1
 
-#line 960 "sgfl.c"
+#line 968 "sgfl.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -1056,9 +1064,20 @@ YY_MALLOC_DECL
 			YY_FATAL_ERROR( "input in flex scanner failed" ); \
 		result = n; \
 		} \
-	else if ( ((result = fread( buf, 1, max_size, yyin )) == 0) \
-		  && ferror( yyin ) ) \
-		YY_FATAL_ERROR( "input in flex scanner failed" );
+	else \
+		{ \
+		errno=0; \
+		while ( (result = fread(buf, 1, max_size, yyin))==0 && ferror(yyin)) \
+			{ \
+			if( errno != EINTR) \
+				{ \
+				YY_FATAL_ERROR( "input in flex scanner failed" ); \
+				break; \
+				} \
+			errno=0; \
+			clearerr(yyin); \
+			} \
+		}
 #endif
 
 /* No semi-colon after return; correct usage is to write "yyterminate();" -
@@ -1104,12 +1123,12 @@ YY_MALLOC_DECL
 YY_DECL
 	{
 	register yy_state_type yy_current_state;
-	register char *yy_cp = NULL, *yy_bp = NULL;
+	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
 #line 64 "./sgf.l"
 
-#line 1113 "sgfl.c"
+#line 1132 "sgfl.c"
 
 	if ( yy_init )
 		{
@@ -1255,7 +1274,7 @@ YY_RULE_SETUP
 #line 97 "./sgf.l"
 YY_FATAL_ERROR( "flex scanner jammed" );
 	YY_BREAK
-#line 1259 "sgfl.c"
+#line 1278 "sgfl.c"
 			case YY_STATE_EOF(INITIAL):
 			case YY_STATE_EOF(value):
 				yyterminate();
@@ -1799,6 +1818,15 @@ YY_BUFFER_STATE b;
 	}
 
 
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#ifndef YY_ALWAYS_INTERACTIVE
+#ifndef YY_NEVER_INTERACTIVE
+extern int isatty YY_PROTO(( int ));
+#endif
+#endif
+#endif
 
 #ifdef YY_USE_PROTOS
 void yy_init_buffer( YY_BUFFER_STATE b, FILE *file )
--- sgfp.h
+++ sgfp.h
@@ -1,11 +1,19 @@
+#ifndef BISON_SGFP_H
+# define BISON_SGFP_H
+
+#ifndef YYSTYPE
 typedef union {
     char ach[ 2 ]; /* property identifier */
     char *pch; /* property value */
     property *pp; /* complete property */
     list *pl; /* nodes, sequences, gametrees */
-} YYSTYPE;
-#define	PROPERTY	257
-#define	VALUETEXT	258
+} yystype;
+# define YYSTYPE yystype
+#endif
+# define	PROPERTY	257
+# define	VALUETEXT	258
 
 
 extern YYSTYPE sgflval;
+
+#endif /* not BISON_SGFP_H */
