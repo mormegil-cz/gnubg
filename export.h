505c47590bf67ff453a45eb57c29342038177e37
thyssen <thyssen>
2002-07-21 19:33:57 +0000

    Output rollout resulst in HTML and text export.

--- export.h
+++ export.h
@@ -72,4 +72,40 @@ getMoveNumber ( const list *plGame, const void *p );
 extern char *
 HTMLFilename ( const char *szBase, const int iGame );
 
+/* misc. output routines used by text and HTML export */
+
+extern char *
+OutputPercents ( const float ar[], const int f );
+
+extern char *
+OutputPercent ( const float r );
+
+extern char *
+OutputMWC ( const float r, const cubeinfo *pci, const int f );
+
+extern char *
+OutputEquity ( const float r, const cubeinfo *pci, const int f );
+
+extern char *
+OutputRolloutContext ( const char *szIndent, const rolloutcontext *prc );
+
+extern char *
+OutputEvalContext ( const evalcontext *pec, const int fChequer );
+
+extern char *
+OutputEquityDiff ( const float r1, const float r2, const cubeinfo *pci );
+
+extern char *
+OutputEquityScale ( const float r, const cubeinfo *pci, 
+                    const cubeinfo *pciBase, const int f );
+
+extern char *
+OutputRolloutResult ( const char *szIndent,
+                      char asz[][ 1024 ],
+                      float aarOutput[][ NUM_ROLLOUT_OUTPUTS ],
+                      float aarStdDev[][ NUM_ROLLOUT_OUTPUTS ],
+                      const cubeinfo aci[],
+                      const int cci,
+                      const int fCubeful );
+
 #endif
