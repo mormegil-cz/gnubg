6cdeb78f6ec9f13a2b8b27f2f6d788237b201d12
thyssen <thyssen>
2002-09-08 19:43:24 +0000

    Option to rotate first two rolls of rollout.

--- eval.h
+++ eval.h
@@ -99,7 +99,8 @@ typedef struct _rolloutcontext {
 
   unsigned int fCubeful : 1; /* Cubeful rollout */
   unsigned int fVarRedn : 1; /* variance reduction */
-  unsigned int fInitial: 1; /* roll out as opening position */
+  unsigned int fInitial: 1;  /* roll out as opening position */
+  unsigned int fRotate : 1;  /* rotate dice of first two rolls */
     
   unsigned short nTruncate; /* truncation */
   unsigned short nTrials; /* number of rollouts */
