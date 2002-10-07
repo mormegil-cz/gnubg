e9d36639be6b8ad233063af9f1b3a30af0442b2a
thyssen <thyssen>
2002-10-07 18:58:31 +0000

    slight modification of interpolation formulae used for error rates above 0.03.

--- analysis.c
+++ analysis.c
@@ -1030,7 +1030,7 @@ absoluteFibsRating ( const float r, const int n ) {
   /* error rate above 0.030 */
   /* use exponential interpolation */
 
-  return 500.0f + 1000.0f * exp ( 0.030f - r );
+  return 500.0f + 1000.0f * exp ( 0.030f - 10.0 * r );
 
 }
 
