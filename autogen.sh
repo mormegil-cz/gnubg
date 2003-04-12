0f9d72fbb6c92b1887cf7db6f4773f594c89fe98
thyssen <thyssen>
2003-04-12 07:20:41 +0000

    touch sounds/Makefile.in (contributed by TAKAHASHI Kaoru)

--- autogen.sh
+++ autogen.sh
@@ -7,9 +7,10 @@ fi
 touch Makefile.in aclocal.m4 config.h.in configure
 touch doc/Makefile.in
 touch ext/Makefile.in
-touch lib/Makefile.in
-touch met/Makefile.in
 touch intl/Makefile.in
-touch po/Makefile.in
+touch lib/Makefile.in
 touch m4/Makefile.in
+touch met/Makefile.in
+touch po/Makefile.in
+touch sounds/Makefile.in
 touch xpm/Makefile.in
