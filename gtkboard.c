9fbb8e9783349f6e748434dd417dcc5fe1e2f875
thyssen <thyssen>
2003-07-26 20:49:37 +0000

    fix typo

--- gtkboard.c
+++ gtkboard.c
@@ -511,7 +511,7 @@ ReturnHits( int anBoard[ 2 ][ 25 ] ) {
       break;
 
   if ( j >= 0 ) {
-    char *pch = g_malloc( 3 * j + 20 );
+    char *pch = g_malloc( 3 * ( j + 1 ) + 20 );
     strcpy( pch, "" );
     for ( i = 0; i <= j; ++i )
       if ( aiHit[ i ] )
