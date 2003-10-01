112ae805bd9feef8c90496bb14d15672b5496ff7
Superfly_Jon <Superfly_Jon>
2003-10-01 08:38:17 +0000

    Fixed 2d board chequer shape setting

--- gtkprefs.c
+++ gtkprefs.c
@@ -706,21 +706,21 @@ static GtkWidget *ChequerPrefs( BoardData *bd, int f ) {
 			       (GtkObject*) ( pwPreview + PI_CHEQUERS0 + f ) );
 
    
-    padjRound = GTK_ADJUSTMENT( gtk_adjustment_new( 1.0 - rdAppearance.rRound, 0, 1,
-						    0.1, 0.1, 0 ) );
-	gtk_signal_connect_object( GTK_OBJECT( padjRound ), "value-changed",
-			       GTK_SIGNAL_FUNC( UpdatePreview ), NULL );
-    pwScale = gtk_hscale_new( padjRound );
-#if GTK_CHECK_VERSION(2,0,0)
-    gtk_widget_set_size_request( pwScale, 100, -1 );
-#else
-    gtk_widget_set_usize ( GTK_WIDGET ( pwScale ), 100, -1 );
-#endif
-    gtk_scale_set_draw_value( GTK_SCALE( pwScale ), FALSE );
-    gtk_scale_set_digits( GTK_SCALE( pwScale ), 2 );
-
 	if (f == 0)
 	{
+		padjRound = GTK_ADJUSTMENT( gtk_adjustment_new( 1.0 - rdAppearance.rRound, 0, 1,
+								0.1, 0.1, 0 ) );
+		gtk_signal_connect_object( GTK_OBJECT( padjRound ), "value-changed",
+					   GTK_SIGNAL_FUNC( UpdatePreview ), NULL );
+		pwScale = gtk_hscale_new( padjRound );
+#if GTK_CHECK_VERSION(2,0,0)
+		gtk_widget_set_size_request( pwScale, 100, -1 );
+#else
+		gtk_widget_set_usize ( GTK_WIDGET ( pwScale ), 100, -1 );
+#endif
+		gtk_scale_set_draw_value( GTK_SCALE( pwScale ), FALSE );
+		gtk_scale_set_digits( GTK_SCALE( pwScale ), 2 );
+
 		pwBox = gtk_hbox_new( FALSE, 0 );
 		gtk_box_pack_start ( GTK_BOX ( pwBox ), gtk_label_new( _("Chequer shape:") ), FALSE, FALSE, 0 );
 		gtk_box_pack_start ( GTK_BOX ( pw ), pwBox, FALSE, FALSE, 4 );
