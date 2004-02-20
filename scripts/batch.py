68ecab30f5c651573834243525e3b63d12d911e2
uid65656 <uid65656>
2004-02-20 14:17:05 +0000

    move gnubg.py to scripts.

--- scripts/batch.py
+++ scripts/batch.py
@@ -1,6 +1,28 @@
+#
+# batch.py -- batch import and analysis of multiple files using gnubg
+#
+# by Jon Kinsey <Jon_Kinsey@hotmail.com>, 2004
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# $Id$
+
 """
-Batch import and analysis of multiple files using gnubg
-  By Jon Kinsey, 2004\n"""
+ batch.py -- batch import and analysis of multiple files using gnubg
+
+ by Jon Kinsey <Jon_Kinsey@hotmail.com>, 2004
+\n"""
 
 import os
 
