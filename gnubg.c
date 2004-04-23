6e3a1ff57f16ede7c4372122691f7907d880f6e5
thyssen <thyssen>
2004-04-23 16:51:08 +0000

    restore list of years (see http://www.gnu.org/prep/maintain_8.html)

--- gnubg.c
+++ gnubg.c
@@ -7861,8 +7861,9 @@ static void real_main( void *closure, int argc, char *argv[] ) {
 #if USE_GTK
     if( fTTY )
 #endif
-      printf( _("GNU Backgammon %s  Copyright 1999-2004 "
-                "Gary Wong.\n"
+      printf( _("GNU Backgammon %s\n"
+                "Copyright 1999, 2000, 2001, 2002, 2003, 2004 "
+                "by Gary Wong.\n"
                 "GNU Backgammon is free software, covered by the GNU "
                 "General Public License\n"
                 "version 2, and you are welcome to change it and/or "
