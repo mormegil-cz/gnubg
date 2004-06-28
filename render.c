da6df93ad61aa0600dbb47b3c64b1b736b10124a
Superfly_Jon <Superfly_Jon>
2004-06-28 09:24:18 +0000

    Small bug in design dialog

--- render.c
+++ render.c
@@ -3027,8 +3027,10 @@ extern int PreferenceCompare(renderdata *prd1, renderdata *prd2)
 			!memcmp(prd1->afDieColour3d, prd2->afDieColour3d, sizeof(prd1->afDieColour3d)) &&
 			MaterialTextCompare(&prd1->ChequerMat[0], &prd2->ChequerMat[0]) &&
 			MaterialCompare(&prd1->ChequerMat[1], &prd2->ChequerMat[1]) &&
-			MaterialCompare(&prd1->DiceMat[0], &prd2->DiceMat[0]) &&
-			MaterialCompare(&prd1->DiceMat[1], &prd2->DiceMat[1]) &&
+			(prd1->afDieColour3d[0] ||
+				MaterialCompare(&prd1->DiceMat[0], &prd2->DiceMat[0])) &&
+			(prd1->afDieColour3d[1] ||
+				MaterialCompare(&prd1->DiceMat[1], &prd2->DiceMat[1])) &&
 			MaterialCompare(&prd1->DiceDotMat[0], &prd2->DiceDotMat[0]) &&
 			MaterialCompare(&prd1->DiceDotMat[1], &prd2->DiceDotMat[1]) &&
 			MaterialCompare(&prd1->CubeMat, &prd2->CubeMat) &&
