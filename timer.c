44d8ab6b82f5ef03e061834fbefec9201c47c2dd
Superfly_Jon <Superfly_Jon>
2005-09-14 21:37:07 +0000

    Added test linux timing code

--- timer.c
+++ timer.c
@@ -62,6 +62,8 @@ double get_time()
 
 #else
 
+#if 1
+
 double get_time()
 {	/* Return elapsed time in milliseconds */
 	struct timeval tv;
@@ -69,5 +71,32 @@ double get_time()
 
 	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
+
+#else
+
+double perFreq = 0;
+
+int setup_timer()
+{
+    perFreq = __get_clockfreq() / 1000.0;
+     return 1;
+}
+
+double get_time()
+{    /* Return elapsed time in milliseconds */
+    if (!perFreq)
+    {
+        if (!setup_timer())
+            return clock() / 1000.0;
+    }
+{
+    unsigned long long int val;
+    __asm__ __volatile__("rdtsc" : "=A" (val) : );
+
+    return val / perFreq;
+}
+}
+
+#endif
 
 #endif
