e5c9f3a5ad0e9cc13ffb4c7b1de153f0dbf071e9
Superfly_Jon <Superfly_Jon>
2006-05-24 19:00:20 +0000

    Flip texture pixmaps when they're loaded so they appear the right way up in opengl

--- board3d/misc3d.c
+++ board3d/misc3d.c
@@ -456,12 +456,12 @@ int LoadTexture(Texture* texture, const char* filename)
 	int n;
 	char *szFile = PathSearch( filename, szDataDirectory );
 	GError *pix_error = NULL;
-	GdkPixbuf *pixbuf;
+	GdkPixbuf *fpixbuf, *pixbuf;
 	
 	if (!szFile)
 		return 0;
  
-	pixbuf = gdk_pixbuf_new_from_file(szFile, &pix_error);
+	fpixbuf = gdk_pixbuf_new_from_file(szFile, &pix_error);
 
 	if (pix_error) {
 		g_print("Failed to open texture: %s\n", szFile );
@@ -470,6 +470,9 @@ int LoadTexture(Texture* texture, const char* filename)
 	}
 
 	free( szFile );
+
+	pixbuf = gdk_pixbuf_flip(fpixbuf, FALSE);
+	g_object_unref(fpixbuf);
 
 	bits = gdk_pixbuf_get_pixels(pixbuf);
 	
