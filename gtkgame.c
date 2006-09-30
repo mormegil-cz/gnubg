3c08ac287db6a1043c3b604dfbc43bd48e54dded
Superfly_Jon <Superfly_Jon>
2006-09-30 09:10:12 +0000

    Fix minor edit bug (labels dissappearing when panels moved)

--- gtkgame.c
+++ gtkgame.c
@@ -1627,11 +1627,6 @@ extern void SetPanelWidth(int size)
 
 extern void SwapBoardToPanel(int ToPanel)
 {	/* Show/Hide panel on right of screen */
-
-	/* Need to hide these, as handle box seems to be buggy and gets confused */
-	gtk_widget_hide(gtk_widget_get_parent(pwMenuBar));
-	gtk_widget_hide(gtk_widget_get_parent(pwToolbar));
-
 	if (ToPanel)
 	{
 		gtk_widget_reparent(pwEventBox, pwPanelGameBox);
@@ -1640,9 +1635,21 @@ extern void SwapBoardToPanel(int ToPanel)
 			gtk_main_iteration();
 		gtk_widget_hide(pwGameBox);
 		SetPanelWidth(panelSize);
+{	/* Hack to sort out widget positions - may be removed if works in later version of gtk */
+		BoardData *bd = BOARD( pwBoard )->board_data;
+		GtkAllocation temp = pwMain->allocation;
+		temp.height++;
+		gtk_widget_size_allocate(pwMain, &temp);
+		temp.height--;
+		gtk_widget_size_allocate(pwMain, &temp);
+}
 	}
 	else
 	{
+		/* Need to hide these, as handle box seems to be buggy and gets confused */
+		gtk_widget_hide(gtk_widget_get_parent(pwMenuBar));
+		gtk_widget_hide(gtk_widget_get_parent(pwToolbar));
+
 		gtk_widget_reparent(pwEventBox, pwGameBox);
 		gtk_widget_show(pwGameBox);
 		while(gtk_events_pending())
@@ -1652,9 +1659,9 @@ extern void SwapBoardToPanel(int ToPanel)
 			panelSize = GetPanelSize();
 			gtk_widget_hide(hpaned);
 		}
+		gtk_widget_show(gtk_widget_get_parent(pwMenuBar));
+		gtk_widget_show(gtk_widget_get_parent(pwToolbar));
 	}
-	gtk_widget_show(gtk_widget_get_parent(pwMenuBar));
-	gtk_widget_show(gtk_widget_get_parent(pwToolbar));
 }
 
 static void MainSize( GtkWidget *pw, GtkRequisition *preq, gpointer p ) {
