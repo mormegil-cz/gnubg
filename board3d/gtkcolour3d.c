43b96c54fe98a1904e3198ec8e05d07a42aa51ee
oysteijo <oysteijo>
2007-04-12 15:26:47 +0000

    gdk_gc_unref->g_object_unref

--- board3d/gtkcolour3d.c
+++ board3d/gtkcolour3d.c
@@ -161,7 +161,7 @@ static void UpdatePreviewBar(const Material* pMat, GdkPixmap *pixmap)
 	gc = gdk_gc_new(pixmap);
 	gdk_draw_rgb_image(pixmap, gc, 0, 0, PREVIEW_WIDTH, PREVIEW_HEIGHT, GDK_RGB_DITHER_MAX,
 					  auch, PREVIEW_WIDTH * 3 );
-	gdk_gc_unref( gc );
+	g_object_unref( gc );
 }
 
 static void UpdateColourPreview(void *notused)
@@ -342,7 +342,7 @@ static gboolean OkClicked(GtkWidget *pw, const UpdateDetails* pDetails)
 	gc = gdk_gc_new(pDetails->pixmap);
 	gdk_draw_rgb_image(pDetails->pixmap, gc, 0, 0, PREVIEW_WIDTH, PREVIEW_HEIGHT, GDK_RGB_DITHER_MAX,
 						auch, PREVIEW_WIDTH * 3 );
-	gdk_gc_unref( gc );
+	g_object_unref( gc );
 
 	gtk_widget_queue_draw(pDetails->preview);
 	UpdatePreview(0);
