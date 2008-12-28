0d20cdbb5f94d4ff7fea317b0f8e618e5fc035ab
c_anthon <c_anthon>
2008-12-28 23:09:25 +0000

    fix createtmpfile

--- util.c
+++ util.c
@@ -24,6 +24,11 @@
 #include <glib.h>
 #include <stdlib.h>
 #include <string.h>
+#include <glib/gstdio.h>
+
+#if HAVE_UNISTD_H
+#include <unistd.h>
+#endif
 
 #ifdef WIN32
 #include <windows.h>
@@ -91,26 +96,21 @@ FILE *fdopen(int, const char *);
 
 extern FILE *GetTemporaryFile(const char *nameTemplate, char **retName)
 {
-    FILE *pf;
-    int tmpd = g_file_open_tmp(nameTemplate, retName, NULL); 
-    if (tmpd < 0)
-	{
-      PrintError("creating temporary file");
-      return NULL;
-    }
+	FILE *pf;
+	int tmpd = g_file_open_tmp(nameTemplate, retName, NULL);
 
-#ifndef WIN32
-	pf = fdopen(tmpd, "w+" );
-#else
-	pf = fopen(*retName, "wb+" );
-#endif
-
-	if (pf == NULL)
-	{
-		g_free(retName);
-		PrintError("opening temporary file");
+	if (tmpd < 0) {
+		PrintError("creating temporary file");
 		return NULL;
 	}
-	else
-		return pf;
+	close(tmpd);
+
+	pf = g_fopen(*retName, "wb+");
+
+	if (pf == NULL) {
+		g_free(retName);
+		PrintError("opening temporary file");
+	}
+
+	return pf;
 }
