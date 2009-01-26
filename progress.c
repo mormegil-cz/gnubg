d1594e627a4e8db4221e58f475b4688b6a37beb2
c_anthon <c_anthon>
2009-01-26 11:11:44 +0000

    print jsd in text rollout with fCubeRollout

--- progress.c
+++ progress.c
@@ -1146,7 +1146,7 @@ TextRolloutProgress( float aarOutput[][ NUM_ROLLOUT_OUTPUTS ],
       else
 	      sprintf( pc, " %d%c", nRank, fStopped ? 's' : 'r' );
 
-      if ( nRank != 1 )
+      if ( nRank != 1 || fCubeRollout )
         sprintf( strchr( pc, 0 ), " %5.3f\n", rJsd );
       else
         strcat( pc, "\n" );
