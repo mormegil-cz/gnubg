435a181b14771dc9726a37e30669731ea8ba1c5f
c_anthon <c_anthon>
2009-03-12 23:51:29 +0000

    other cmake files

---
+++ config.h.cmake
@@ -0,0 +1,102 @@
+/* Define if you want to disable the SSE cpu test */
+/* #define DISABLE_SSE_TEST 1 */
+
+/* Define if you wish to use glib threads */
+#define GLIB_THREADS 1
+
+/* Define if you want to use libcairo */
+#cmakedefine HAVE_CAIRO 1
+
+/* Define if you have freetype2 */
+#cmakedefine HAVE_FREETYPE 1
+
+/* Define if you want to use gstreamer */
+#cmakedefine HAVE_GSTREAMER 1
+
+/* Define to 1 if you have the `gmp' library (-lgmp). */
+#cmakedefine HAVE_LIBGMP 1
+
+/* Define if you have libpng */
+#cmakedefine HAVE_LIBPNG 1
+
+/* Define if you have libreadline */
+#cmakedefine HAVE_LIBREADLINE 1
+
+/* Define if you have libxml2. */
+#cmakedefine HAVE_LIBXML2 1
+
+/* Define if you want to use pangocairo */
+#cmakedefine HAVE_PANGOCAIRO 1
+
+/* Define to 1 if you have the `random' function. */
+#cmakedefine HAVE_RANDOM 1
+
+/* Define to 1 if you have the `setpriority' function. */
+#cmakedefine HAVE_SETPRIORITY 1
+
+/* Define to 1 if you have the `sigaction' function. */
+#cmakedefine HAVE_SIGACTION 1
+
+/* Define to 1 if you have the `sigvec' function. */
+#cmakedefine HAVE_SIGVEC 1
+
+/* Define if the system supports AF_LOCAL sockets. */
+#cmakedefine HAVE_SOCKETS 1
+
+/* Define if you want to use sqlite */
+#cmakedefine HAVE_SQLITE 1
+
+/* Define to 1 if you have the `strptime' function. */
+#cmakedefine HAVE_STRPTIME 1
+
+/* Define to 1 if you have the <sys/resource.h> header file. */
+#cmakedefine HAVE_SYS_RESOURCE_H 1
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#cmakedefine HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#cmakedefine HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "gnubg"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "bug-gnubg@gnu.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GNU Backgammon"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GNU Backgammon 0.90.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gnubg"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.90.0"
+
+/* Define if you want to use the 3d boards */
+#cmakedefine USE_BOARD3D 1
+
+/* Define if you want to use the gtk gui */
+#cmakedefine USE_GTK 1
+
+/* Define if you want to have multithread support */
+#cmakedefine USE_MULTITHREAD 1
+
+/* Define if you want to use Python inside gnubg */
+#cmakedefine USE_PYTHON 1
+
+/* Define if you want to compile with SSE support */
+#cmakedefine USE_SSE_VECTORIZE 1
+
+/* Version number of package */
+#define VERSION "0.90.0"
+
