ecf42acf83e102690777b4c3b825ee484bcff92e
mdpetch <mdpetch>
2009-04-06 05:52:20 +0000

    Fixed problem with Python2.4. Py_ssize_t is not defined prior to 2.5 .

--- gnubgmodule.h
+++ gnubgmodule.h
@@ -30,6 +30,11 @@
 #endif
 #endif
 #include <Python.h>
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+#endif
 #endif
 
 extern void PythonInitialise(char *argv0);
