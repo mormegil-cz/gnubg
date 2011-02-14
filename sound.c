6377ad4619760097e7be3075eb637796fc64fb78
c_anthon <c_anthon>
2011-02-14 17:07:14 +0000

    fix sound in cl (unix)

--- sound.c
+++ sound.c
@@ -208,6 +208,9 @@ void
 playSoundFile (char *file, /*lint -e{715}*/gboolean sync)
 {
     GError *error = NULL;
+#if HAVE_CANBERRA
+    static ca_context *canberracontext = NULL;
+#endif
     if (!g_file_test(file, G_FILE_TEST_EXISTS))
     {
             outputf(_("The sound file (%s) doesn't exist.\n"), file);
@@ -257,7 +260,16 @@ playSoundFile (char *file, /*lint -e{715}*/gboolean sync)
 #elif defined(__APPLE__)
 	PlaySound_QuickTime (file);
 #elif HAVE_CANBERRA
-	ca_context_play(ca_gtk_context_get(), 0, CA_PROP_MEDIA_FILENAME, file, NULL);
+	if (!canberracontext)
+	{
+#if USE_GTK
+		if (fX)
+			canberracontext = ca_gtk_context_get();
+		else
+#endif
+			ca_context_create(&canberracontext);
+	}
+	ca_context_play(canberracontext, 0, CA_PROP_MEDIA_FILENAME, file, NULL);
 #endif
 }
 
