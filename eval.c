f0c39f8bea39f49f7898ff56056d66f8f27d0ffb
plm <plm>
2011-05-13 18:22:45 +0000

    Computation of baseInputs() with SSE

--- eval.c
+++ eval.c
@@ -1486,143 +1486,7 @@ CalculateRaceInputs(const TanBoard anBoard, float inputs[])
   }
 }
 
-#if 0
-float inpvec[16][4] = { 
-/*  0 */				{ 0.0, 0.0, 0.0, 0.0 },
-/*  1 */				{ 1.0, 0.0, 0.0, 0.0 },
-/*  2 */				{ 0.0, 1.0, 0.0, 0.0 },
-/*  3 */				{ 0.0, 0.0, 1.0, 0.0 },
-/*  4 */				{ 0.0, 0.0, 1.0, 0.5 },
-/*  5 */				{ 0.0, 0.0, 1.0, 1.0 },
-/*  6 */				{ 0.0, 0.0, 1.0, 1.5 },
-/*  7 */				{ 0.0, 0.0, 1.0, 2.0 },
-/*  8 */				{ 0.0, 0.0, 1.0, 2.5 },
-/*  9 */				{ 0.0, 0.0, 1.0, 3.0 },
-/* 10 */				{ 0.0, 0.0, 1.0, 3.5 },
-/* 11 */				{ 0.0, 0.0, 1.0, 4.0 },
-/* 12 */				{ 0.0, 0.0, 1.0, 4.5 },
-/* 13 */				{ 0.0, 0.0, 1.0, 5.0 },
-/* 14 */				{ 0.0, 0.0, 1.0, 5.5 },
-/* 15 */				{ 0.0, 0.0, 1.0, 6.0 } };
-
-float inpvecb[16][4] = { 
-/*  0 */				{ 0.0, 0.0, 0.0, 0.0 },
-/*  1 */				{ 1.0, 0.0, 0.0, 0.0 },
-/*  2 */				{ 1.0, 1.0, 0.0, 0.0 },
-/*  3 */				{ 1.0, 1.0, 1.0, 0.0 },
-/*  4 */				{ 1.0, 1.0, 1.0, 0.5 },
-/*  5 */				{ 1.0, 1.0, 1.0, 1.0 },
-/*  6 */				{ 1.0, 1.0, 1.0, 1.5 },
-/*  7 */				{ 1.0, 1.0, 1.0, 2.0 },
-/*  8 */				{ 1.0, 1.0, 1.0, 2.5 },
-/*  9 */				{ 1.0, 1.0, 1.0, 3.0 },
-/* 10 */				{ 1.0, 1.0, 1.0, 3.5 },
-/* 11 */				{ 1.0, 1.0, 1.0, 4.0 },
-/* 12 */				{ 1.0, 1.0, 1.0, 4.5 },
-/* 13 */				{ 1.0, 1.0, 1.0, 5.0 },
-/* 14 */				{ 1.0, 1.0, 1.0, 5.5 },
-/* 15 */				{ 1.0, 1.0, 1.0, 6.0 } };
-
-extern void
-baseInputs(const TanBoard anBoard, float arInput[])
-{
-  int i = 3;
-
-	int *pB = &anBoard[0][0];
-	float *pInput = &arInput[0];
-	register __m128 vec0;
-	register __m128 vec1;
-	register __m128 vec2;
-	register __m128 vec3;
-	register __m128 vec4;
-	register __m128 vec5;
-	register __m128 vec6;
-	register __m128 vec7;
-	
-	while ( i-- ){
-					vec0 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput , vec0 );
-					vec1 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4 , vec1 );
-					vec2 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4 , vec2 );
-					vec3 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4 , vec3 );
-					vec4 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4 , vec4 );
-					vec5 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4 , vec5 );
-					vec6 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4 , vec6 );
-					vec7 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4 , vec7 );
-					pInput += 4;
-	}
-
-	/* bar */
-	vec0 = _mm_load_ps(inpvecb[*pB++]);
-	_mm_store_ps(pInput, vec0 );
-	pInput += 4;
-
-	i = 3;
-	while ( i-- ){
-					vec0 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput , vec0 );
-					vec1 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4, vec1 );
-					vec2 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4, vec2 );
-					vec3 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4, vec3 );
-					vec4 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4, vec4 );
-					vec5 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4, vec5 );
-					vec6 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4, vec6 );
-					vec7 = _mm_load_ps(inpvec[*pB++]);
-					_mm_store_ps(pInput += 4, vec7 );
-					pInput += 4;
-	}
-	
-	/* bar */
-	vec0 = _mm_load_ps(inpvecb[*pB]);
-	_mm_store_ps(pInput, vec0 );
-	
-	return;
-}
-#else
-extern void
-baseInputs(const TanBoard anBoard, float arInput[])
-{
-  int j, i;
-    
-  for(j = 0; j < 2; ++j ) {
-    float* afInput = arInput + j * 25*4;
-    const unsigned int* board = anBoard[j];
-    
-    /* Points */
-    for( i = 0; i < 24; i++ ) {
-      int nc = board[ i ];
-      
-      afInput[ i * 4 + 0 ] = (nc == 1) ? 1.0f : 0.0f;
-      afInput[ i * 4 + 1 ] = (nc == 2) ? 1.0f : 0.0f;
-      afInput[ i * 4 + 2 ] = (nc >= 3) ? 1.0f : 0.0f;
-      afInput[ i * 4 + 3 ] = nc > 3 ? ( nc - 3 ) / 2.0f : 0.0f;
-    }
-
-    /* Bar */
-    {
-      int nc = board[ 24 ];
-      
-      afInput[ 24 * 4 + 0 ] = (nc >= 1) ? 1.0f : 0.0f;
-      afInput[ 24 * 4 + 1 ] = (nc >= 2) ? 1.0f : 0.0f;
-      afInput[ 24 * 4 + 2 ] = (nc >= 3) ? 1.0f : 0.0f;
-      afInput[ 24 * 4 + 3 ] = nc > 3 ? ( nc - 3 ) / 2.0f : 0.0f;
-    }
-  }
-}
-#endif 
+/* baseInputs() is now in lib/inputs.c */
 
 static void
 menOffAll(const unsigned int* anBoard, float* afInput)
@@ -5622,8 +5486,8 @@ static void FindBestMoveInEval(NNState * nnStates, int const nDice0, int const n
 
 	for (i = 0; i < ml.cMoves; i++) {
 		positionclass pc;
-		float arInput[200];
-		float arOutput[5];
+		SSE_ALIGN(float arInput[200]);
+		float arOutput[NUM_OUTPUTS];
 		evalcache ec;
 		unsigned long l;
 		/* declared volatile to avoid wrong compiler optimization
