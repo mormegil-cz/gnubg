3563d29a979c105cfa3b7d77ef6d3cafeff987fb
mdpetch <mdpetch>
2011-05-18 00:40:03 +0000

    Fixes to allow compilation on Win32 platform

--- board3d/fun3d.h
+++ board3d/fun3d.h
@@ -18,6 +18,19 @@
 #include "gtkboard.h"
 #include "analysis.h"
 
+#if defined(WIN32)
+/* MS gl.h needs windows.h to be included first */
+#include <windows.h>
+#endif
+
+#if defined(USE_APPLE_OPENGL)
+#include <gl.h>
+#include <glu.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
+
 #if defined(HAVE_GL_GLX_H)
 #include <GL/glx.h>     /* x-windows file */
 #endif
